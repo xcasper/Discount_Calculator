1c377e35ff1b7aa6ded788e02a744d52c8f66d37
Craig <cgleckman@gmail.com>
2014-02-25 10:44:08 -0700

    Unit 2, Lesson 2, Part 5 - Milestone 5
    Fix up script to return proper discount regardless of type

--- Discount_Calculator.py
+++ Discount_Calculator.py
@@ -4,7 +4,9 @@ class Discount_Calculator(object):
         if discount_type == 'percent':
             percentage_discount = float(discount_amt) / 100
             discount = float(total) * percentage_discount
-            return discount
+            
         elif discount_type == 'absolute':
-            print "Total discount is: ", discount_amt
-            return total - discount_amt
+            discount = discount_amt
+            
+        return discount
+        
