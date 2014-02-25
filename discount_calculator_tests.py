d0e63749ded8045e59de68f281999db7f63e69b3
Craig <cgleckman@gmail.com>
2014-02-24 21:15:55 -0700

    Module 2 - Thinkful
    Unit 2, Lesson 2, Part 5
    
    made tests number dependent per instructions and added an additional test.

--- discount_calculator_tests.py
+++ discount_calculator_tests.py
@@ -2,7 +2,13 @@ import unittest
 from discount_calculator import Discount_Calculator
 
 class DiscountCalculatorTests(unittest.TestCase):
-    def test_percentage_discount(self):
+    def test_10_percentage_discount(self):
         discount_calculator = Discount_Calculator()
         result = discount_calculator.calculate(100, 10, 'percent')
         self.assertEqual(10.0, result)
+    def test_15_percentage_discount(self):
+        discount_calculator = Discount_Calculator()
+        result = discount_calculator.calculate(100, 15, 'percent')
+        self.assertEqual(15.0, result)
+    
+    
