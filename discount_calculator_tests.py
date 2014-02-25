4f1f30ac5fd62eb0840741a103185048c13ea094
Craig <cgleckman@gmail.com>
2014-02-25 10:45:02 -0700

    Unit 2, Lesson 2, Part 5 - Milestone 5
    Fix up script to return proper discount regardless of type - added test for 5 dollar (absolute).

--- discount_calculator_tests.py
+++ discount_calculator_tests.py
@@ -6,9 +6,13 @@ class DiscountCalculatorTests(unittest.TestCase):
         discount_calculator = Discount_Calculator()
         result = discount_calculator.calculate(100, 10, 'percent')
         self.assertEqual(10.0, result)
+        
     def test_15_percentage_discount(self):
         discount_calculator = Discount_Calculator()
         result = discount_calculator.calculate(100, 15, 'percent')
         self.assertEqual(15.0, result)
-    
-    
+
+    def test_five_dollar_discount(self):
+        discount_calculator = Discount_Calculator()
+        result = discount_calculator.calculate(250, 5, 'absolute')
+        self.assertEqual(5, result)
