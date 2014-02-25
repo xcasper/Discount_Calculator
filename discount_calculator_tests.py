1ff47f3f4f08aca38e2e3b4dce29cc2f384774cc
Craig <cgleckman@gmail.com>
2014-02-25 11:05:42 -0700

    Unit 2, Lesson 2, Part 5 - Milestone 5
    (previous was milestone 4 - woops)
    
    added checks for reasonable inputs (check floats work)

--- discount_calculator_tests.py
+++ discount_calculator_tests.py
@@ -16,3 +16,17 @@ class DiscountCalculatorTests(unittest.TestCase):
         discount_calculator = Discount_Calculator()
         result = discount_calculator.calculate(250, 5, 'absolute')
         self.assertEqual(5, result)
+
+    def test_invalid_discount_type(self):
+        discount_calculator = Discount_Calculator()
+        self.assertRaises(ValueError, discount_calculator.calculate, 250, 5, 'random')
+         
+    def floating_point_percentage_discount_test(self):
+        discount_calculator = Discount_Calculator()
+        result = discount_calculator.calculate(100.0, 10.0, 'percent')
+        self.assertEqual(10.0, result)
+        
+    def floating_point_absolute_discount_test(self):
+        discount_calculator = Discount_Calculator()
+        result = discount_calculator.calculate(250.0, 5.0, 'absolute')
+        self.assertEqual(5.0, result)
