607fff9a02199394729ddb38d87a69254a34bed1
Craig <cgleckman@gmail.com>
2014-02-25 11:17:04 -0700

    Unit 2, Lesson 2, Part 5 - Milestone 6
    Added further tests to make sure that percent discounts do not exceed 100% and absolute do not exceed total of order

--- Discount_Calculator.py
+++ Discount_Calculator.py
@@ -2,10 +2,14 @@ class Discount_Calculator(object):
 
     def calculate(self, total, discount_amt, discount_type):
         if discount_type == 'percent':
+            if discount_amt > 100:
+                raise ValueError("Percentage Discount Cannot Exceed 100%")
             percentage_discount = float(discount_amt) / 100
             discount = float(total) * percentage_discount
             
         elif discount_type == 'absolute':
+            if discount_amt > total:
+                raise ValueError("Aboslute Discount Cannot Exceed Order Total.")
             discount = discount_amt
 
         else:
